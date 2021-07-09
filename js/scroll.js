d0c164304b119ae3a36f8b83759594e85add658d
jasminebrown2308 <jasminebrown2308@gmail.com>
2021-07-09 19:45:19 +0100

    started cv

--- js/scroll.js
+++ js/scroll.js
@@ -1,21 +1,24 @@
 var showMenu;
 
+/* When document loads, initialise variable and assign function to make menu appear when hovering over the top of the screen */
 $( function() {
 	showMenu = false;
 	$("#menu-sensor").mouseenter( function () {
 		if (showMenu) {
-			$("#menu").css("transition", "transform 0.2s ease");
+			$("#menu").css("transition", "transform 0.3s ease");
 			$("#menu").css("transform", "translateY(-2vh)");
 		}
 	});
 });
 
+/* When scrolling, check if the user is on the main section (in which case the menu should be at the top and scroll with the page) 
+or another section (menu should be hidden unless the user hovers at the top of the screen) */
 $(window).scroll(function() {
 	if ($(window).scrollTop() > window.innerHeight) {
 		showMenu = true;
-		var menuHeight = (window.innerHeight * 0.11) + 24;
+		var menuHeight = (window.innerHeight * 0.11) + 24; // the height of the menu is 11vh plus the 24px font
 		$("#menu").css("position", "fixed");
-		$("#menu").css("transform", "translateY(-" + menuHeight + "px)");
+		$("#menu").css("transform", "translateY(-" + menuHeight + "px)"); // moves it off the top of the screen
 	} else {
 		showMenu = false;
 		$("#menu").css("position", "absolute");
@@ -24,6 +27,7 @@ $(window).scroll(function() {
 	}
 });
 
+/* Scroll to the specified section of the page */
 function jumpToSection(elementID) {
 	const element = $("#"+elementID)[0]
 	element.scrollIntoView();
